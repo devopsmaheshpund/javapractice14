da829ecc27f473027fbb6af21726b012002ef76c
Mahesh <devopsmaheshpund@gmail.com>
2025-06-20 10:20:49 +0530

    first commit

---
+++ P6_ASCIIOfAtoZ.java
@@ -0,0 +1,12 @@
+package javapractice14;
+
+import java.util.Scanner;
+
+public class P6_ASCIIOfAtoZ {
+	public static void main(String args[]) {
+		for (char c = 'a'; c <= 'z'; c++) {
+			int ascii = c;
+			System.out.println(ascii);
+		}
+	}
+}
